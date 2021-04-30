5fe6fe8f0afbf4deeb40a2b273fe75fea399ac12
johnny <jal.johnny13@mail.com>
2021-04-30 20:14:47 -0300

    login com sucesso

--- BlogPessoal/src/app/app-routing.module.ts
+++ BlogPessoal/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { EntrarComponent } from './entrar/entrar.component';
 import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CadastrarComponent } from './cadastrar/cadastrar.component';
+import { InicioComponent } from './inicio/inicio.component';
 
 
 const routes: Routes = [
@@ -9,7 +10,9 @@ const routes: Routes = [
 {path: '', redirectTo: 'entrar', pathMatch: 'full'},
 
 {path:'entrar', component: EntrarComponent},
-{path: 'cadastrar', component: CadastrarComponent}
+{path: 'cadastrar', component: CadastrarComponent},
+
+{ path: 'inicio', component: InicioComponent}
 
 ];
 
--- BlogPessoal/src/app/cadastrar/cadastrar.component.ts
+++ BlogPessoal/src/app/cadastrar/cadastrar.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { User } from '../model/User';
+import { UserLogin } from '../model/userLogin';
+import { AuthService } from '../service/auth.service'
 
 @Component({
   selector: 'app-cadastrar',
@@ -7,9 +11,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CadastrarComponent implements OnInit {
 
-  constructor() { }
+  userLogin: UserLogin = new UserLogin
+  user: User = new User
 
-  ngOnInit(): void {
+  confirmarSenha: string
+  tipoUsuario: string
+
+
+  constructor(
+    private AuthService: AuthService,
+    private router: Router
+  ) { }
+
+  ngOnInit()  {
+
+    window.scroll(0,0)
+
   }
+
+  confirmSenha(event: any) {
+    this.confirmarSenha = event.target.value
+
+  }
+   tipoUser(event: any){
+     this.tipoUsuario = event.target.value
+
+   }
+
+   cadastrar(){
+     this.user.tipo = this.tipoUsuario
+
+     if(this.user.senha !=this.confirmarSenha){
+       alert('As senhas estão incorretas.')
+  }else{ 
+    this.AuthService.cadastrar(this.user).subscribe((resp: User) => {
+    this.user = resp
+    this.router.navigate(['/entrar'])
+
+    alert('Usuario cadastrado com sucesso!')
+  })
+
+  }
+
+   }
 
 }
