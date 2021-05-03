3feb1a16b55ebca2b2fc3e25ad2c35bf5fb3efdc
johnny <jal.johnny13@mail.com>
2021-05-03 16:52:57 -0300

    componente-inicio

--- BlogPessoal/src/app/service/auth.service.ts
+++ BlogPessoal/src/app/service/auth.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.prod';
 import { User } from '../model/User';
 
 import { UserLogin } from '../model/userLogin';
@@ -15,7 +16,7 @@ export class AuthService {
   ) { }
 
   entrar(userLogin: UserLogin): Observable<UserLogin>{
-    return this.http.post<UserLogin>('http://localhost:8080/usuarios/logar' , userLogin)
+    return this.http.post<UserLogin>('http://localhost:8080/usuarios/logar', userLogin)
 
 
   }
@@ -23,6 +24,15 @@ export class AuthService {
   cadastrar(User: User): Observable<User>{
     return this.http.post<User>('http://localhost:8080/usuarios/cadastrar', User)
 
+  }
 
+  logado(){
+    let ok = false
+
+    if(environment.token != ''){
+      ok = true
+    }
+
+    return  ok
   }
 }
